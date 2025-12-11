128b63ad82eadcba47c5d81a3dd71debfb33fc48
Rishabh <rishabhdubey1472004@gmail.com>
2025-12-11 22:16:00 +0530

    solved a question

---
+++ src/main/java/BoatsToSavePeople.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int i=0;
+        int j=people.length-1;
+        int count=0;
+
+        while(i<=j){
+
+            if(people[i]+people[j]<=limit) {
+                j--;
+            }
+
+            i++;
+            count++;
+        }
+
+        return count;
+
+    }
+
+
+}

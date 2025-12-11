6ef037f8958c2a5dee7d90eb0940fead2eb6af33
Rishabh <rishabhdubey1472004@gmail.com>
2025-12-11 19:49:50 +0530

    initial commit

--- src/main/java/MirrorBinaryTree.java
+++ src/main/java/MirrorBinaryTree.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
 
 public class MirrorBinaryTree {
+
+    void mirror(Node root) {
+
+        if(root==null) return;
+
+        Node temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+
+
+        mirror(root.left);
+        mirror(root.right);
+
+    }
+    static class Node{
+        int data;
+        Node left,right;
+
+        public Node(int item){
+            this.data=item;
+            left=right=null;
+        }
+    }
 }

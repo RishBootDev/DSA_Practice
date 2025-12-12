1335edf681eed316e20ebba50a90896241d30dc7
Rishabh <rishabhdubey1472004@gmail.com>
2025-12-12 23:40:17 +0530

    solved a question

--- src/main/java/MaximumBinaryTree.java
+++ src/main/java/MaximumBinaryTree.java
@@ -46,4 +46,14 @@ public class MaximumBinaryTree {
             this.right = right;
         }
     }
+
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null || val > root.val) {
+            TreeNode n = new TreeNode(val);
+            n.left = root;
+            return n;
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
 }

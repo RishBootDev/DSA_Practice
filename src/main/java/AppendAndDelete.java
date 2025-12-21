1c79f6ae4c7a007535c3f777825d1637e9c5cf31
Rishabh <rishabhdubey1472004@gmail.com>
2025-12-21 20:25:28 +0530

    solved a question

--- src/main/java/AppendAndDelete.java
+++ src/main/java/AppendAndDelete.java
@@ -2,7 +2,6 @@ public class AppendAndDelete {
 
     public static String appendAndDelete(String s, String t, int k) {
 
-
         int len=Math.max(s.length(),t.length());
         int temp=0;
         for (int i = 0; i <len ; i++) {

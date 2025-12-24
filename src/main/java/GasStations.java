65f6960a75c9f0575ca4a257af9062ad34e0e3ed
Rishabh <rishabhdubey1472004@gmail.com>
2025-12-24 12:15:24 +0530

    solved a question

--- src/main/java/GasStations.java
+++ src/main/java/GasStations.java
@@ -28,4 +28,26 @@ public class GasStations {
 
         return -1;
     }
+    public static int canCompleteCircuitOptimized(int[] gas, int[] cost) {
+
+        int totalGas = 0;
+        int currentGas = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+
+            int diff = gas[i] - cost[i];
+
+            totalGas += diff;
+            currentGas += diff;
+
+            if (currentGas < 0) {
+                start = i + 1;
+                currentGas = 0;
+            }
+        }
+
+        return totalGas >= 0 ? start : -1;
+    }
+
 }

007c83712acbb833456aa01a8ea95c13fbd90588
Rishabh <rishabhdubey1472004@gmail.com>
2025-12-25 18:21:36 +0530

    solved a question

---
+++ src/main/java/MaximizeHappiness.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MaximizeHappiness {
+
+    public long maximumHappinessSum(int[] happiness, int k) {
+
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int c: happiness) pq.add(c);
+
+        int temp=0;
+        long ans=0;
+        while(k-->0){
+
+            int get=pq.poll()-temp;
+
+            if(get<0) get=0;
+
+            ans+=get;
+            temp++;
+        }
+
+        return ans;
+    }
+}

1fa3b05bb2459fabc4948bf85632ea4f3ca64cd8
Rishabh <rishabhdubey1472004@gmail.com>
2026-01-07 19:12:11 +0530

    solved a question

---
+++ src/main/java/ConsecutiveBinaryNumbers.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+
+public class ConsecutiveBinaryNumbers {
+
+    private static final int MOD = 1_000_000_007;
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(10));
+    }
+
+
+    public int concatenatedBinary(int n) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <=n; i++) {
+            sb.append(Integer.toBinaryString(i));
+        }
+
+        BigInteger bb = new BigInteger(sb.toString(),2);
+        BigInteger ans = bb.mod(new BigInteger(String.valueOf(MOD)));
+
+        return ans.intValue();
+    }
+}

b4d527bde240cb78e7ca0c8f6a2d79a2a9b58960
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-06 23:26:01 +0530

    solved a question

---
+++ src/main/java/MinimumRemovalsToBalanceArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class MinimumRemovalsToBalanceArray {
+
+
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int left = 0;
+        int maxWindow = 0;
+
+        for (int right = 0; right < n; right++) {
+            while ((long) nums[right] > (long) nums[left] * k) {
+                left++;
+            }
+            maxWindow = Math.max(maxWindow, right - left + 1);
+        }
+
+        return n - maxWindow;
+    }
+}

9136ac5433a641efc8b98451bf64b12b735f439b
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-08 10:14:59 +0530

    solved a question

--- src/main/java/FindThePrefixCommonArray.java
+++ src/main/java/FindThePrefixCommonArray.java
@@ -24,4 +24,31 @@ public class FindThePrefixCommonArray {
         return ans;
 
     }
+
+    public int[] findThePrefixCommonArrayOptimized(int[] arr, int[] barr) {
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        int ans [] = new int[arr.length];
+
+        set1.add(arr[0]);
+        set2.add(barr[0]);
+        if(set2.contains(arr[0])) ans[0]++;
+        if(set1.contains(barr[0])) ans[0]++;
+
+
+        for (int i = 1; i < arr.length; i++) {
+
+            set1.add(arr[i]);
+            set2.add(barr[i]);
+
+            int count =0;
+            if(set2.contains(arr[i])) count++;
+
+            if(set1.contains(barr[i])) count++;
+
+            ans [i] = count+ans[i-1];
+        }
+        return ans;
+    }
 }

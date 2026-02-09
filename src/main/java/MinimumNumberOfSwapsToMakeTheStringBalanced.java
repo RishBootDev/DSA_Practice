9dd7fad879f40235ba28543b70d4244e725c47f4
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-09 17:45:57 +0530

    solved a question

---
+++ src/main/java/MinimumNumberOfSwapsToMakeTheStringBalanced.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class MinimumNumberOfSwapsToMakeTheStringBalanced {
+
+
+        public int minSwaps(String s) {
+
+            Stack<Character> stack = new Stack<>();
+            int swaps = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                char ch = s.charAt(i);
+                if (ch == '[') {
+                    stack.push(ch);
+                }
+                else {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    } else {
+                        swaps++;
+                    }
+                }
+            }
+
+            return (swaps + 1) / 2;
+        }
+
+
+}

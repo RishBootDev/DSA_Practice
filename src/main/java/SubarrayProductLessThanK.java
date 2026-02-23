8da9f1187ba20d8ab67076490969cc385d1f55a8
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-23 14:46:59 +0530

    solved a question

---
+++ src/main/java/SubarrayProductLessThanK.java
@@ -0,0 +1,23 @@
+public class SubarrayProductLessThanK {
+
+    public long countSubArrayProductLessThanK(long a[], int n, long k) {
+
+        long p = 1;
+        long res = 0;
+        for (int start = 0, end = 0; end < n; end++) {
+
+            p *= a[end];
+
+            while (start < end && p >= k)
+                p /= a[start++];
+
+            if (p < k) {
+                int len = end - start + 1;
+                res += len;
+            }
+        }
+
+        return res;
+    }
+
+}

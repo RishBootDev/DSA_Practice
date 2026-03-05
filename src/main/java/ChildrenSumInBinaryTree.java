aebab3b82404f65ba56e284aefed61483cccde3d
Rishabh <rishabhdubey1472004@gmail.com>
2026-03-06 00:50:05 +0530

    solved a question

---
+++ src/main/java/ChildrenSumInBinaryTree.java
@@ -0,0 +1,34 @@
+public class ChildrenSumInBinaryTree {
+
+
+    static class Node{
+        int data;
+        Node left,right;
+
+        Node(int key)
+        {
+            data = key;
+            left = right = null;
+        }
+    }
+
+    public boolean isSumProperty(Node root) {
+
+        if (root == null) return true;
+        if (root.left == null && root.right == null) return true;
+
+        int left = 0;
+        int right = 0;
+        if (root.left != null)
+            left = root.left.data;
+
+        if (root.right != null)
+            right = root.right.data;
+
+        if (left + right != root.data)
+            return false;
+
+        return isSumProperty(root.left) &&
+                isSumProperty(root.right);
+    }
+}

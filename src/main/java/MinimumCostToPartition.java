9bb69b05a811f410dd25c0176e987ff58ace7928
Rishabh <rishabhdubey1472004@gmail.com>
2026-03-08 12:30:07 +0530

    solved a question

---
+++ src/main/java/MinimumCostToPartition.java
@@ -0,0 +1,40 @@
+public class MinimumCostToPartition {
+
+    public long minCost(String s, int encCost, int flatCost) {
+        return solve(s, 0, s.length() - 1, encCost, flatCost);
+    }
+
+    public long solve(String s, int l, int r, int encCost, int flatCost) {
+
+        int length = r - l + 1;
+
+        int ones = 0;
+        for (int i = l; i <= r; i++) {
+            if (s.charAt(i) == '1') {
+                ones++;
+            }
+        }
+
+        long directCost;
+
+        if (ones == 0) {
+            directCost = flatCost;
+        } else {
+            directCost = (long) length * ones * encCost;
+        }
+
+        long ans = directCost;
+
+        if (length % 2 == 0) {
+
+            int mid = (l + r) / 2;
+
+            long splitCost = solve(s, l, mid, encCost, flatCost) + solve(s, mid + 1, r, encCost, flatCost);
+
+            ans = Math.min(ans, splitCost);
+        }
+
+        return ans;
+    }
+}
+

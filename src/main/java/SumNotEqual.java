7256c1a5a1e7d90401a87b5642fdfffb6c4f7f86
Rishabh <rishabhdubey1472004@gmail.com>
2026-03-11 20:30:35 +0530

    solved a question

---
+++ src/main/java/SumNotEqual.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class SumNotEqual {
+
+
+    static class Pair {
+        int val, idx;
+        Pair(int v, int i){
+            val = v;
+            idx = i;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+
+            int n = sc.nextInt();
+            Pair[] arr = new Pair[n];
+
+            boolean allZero = true;
+
+            for(int i=0;i<n;i++){
+                int x = sc.nextInt();
+                arr[i] = new Pair(x, i+1);
+                if(x != 0) allZero = false;
+            }
+
+            if(allZero){
+                System.out.println(-1);
+                continue;
+            }
+
+            Arrays.sort(arr, Comparator.comparingInt(a -> a.val));
+
+            int i = arr[n-1].idx;
+            int j = arr[n-2].idx;
+            int k = arr[0].idx;
+
+            System.out.println(i + " " + j + " " + k);
+        }
+    }
+}

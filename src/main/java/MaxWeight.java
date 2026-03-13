bbc323ef85f9729eded9f3b863765dc63e85dcc1
Rishabh <rishabhdubey1472004@gmail.com>
2026-03-13 12:38:44 +0530

    solved a question

--- src/main/java/MaxWeight.java
+++ src/main/java/MaxWeight.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MaxWeight {
+
+    public int maxWeightCell(int[] exits) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < exits.length; i++) {
+            map.put(i, 0);
+        }
+        for (int i = 0; i < exits.length; i++) {
+
+            if (exits[i] != -1) {
+                map.put(exits[i], map.getOrDefault(exits[i], 0) + i);
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        int result = -1;
+
+        for (Map.Entry<Integer, Integer> ent : map.entrySet()) {
+
+            int cell = ent.getKey();
+            int weight = ent.getValue();
+
+            if (weight > max || (weight == max && cell > result)) {
+                max = weight;
+                result = cell;
+            }
+        }
+
+        return result;
+
+    }
 }

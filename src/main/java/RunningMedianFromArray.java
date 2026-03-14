a0faa63cbabcc0eb878413f776fc5fc1c3464115
Rishabh <rishabhdubey1472004@gmail.com>
2026-03-14 23:48:10 +0530

    solved a question

---
+++ src/main/java/RunningMedianFromArray.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class RunningMedianFromArray {
+
+    public static List<Double> runningMedian(List<Integer> a) {
+
+        List<Double> ans = new ArrayList<>();
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : a) {
+
+            maxHeap.add(num);
+            minHeap.add(maxHeap.poll());
+
+            if (minHeap.size() > maxHeap.size()) {
+                maxHeap.add(minHeap.poll());
+            }
+
+            if (maxHeap.size() == minHeap.size()) {
+                ans.add((maxHeap.peek() + minHeap.peek()) / 2.0);
+            } else {
+                ans.add((double) maxHeap.peek());
+            }
+        }
+
+        return ans;
+    }
+}

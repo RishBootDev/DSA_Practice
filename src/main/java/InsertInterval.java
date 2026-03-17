c61b305b093b9db8191b42bb1b954766d1a7cb1a
Rishabh <rishabhdubey1472004@gmail.com>
2026-03-17 22:03:01 +0530

    solved a question

---
+++ src/main/java/InsertInterval.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertInterval {
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        int a = newInterval[0];
+        int b = newInterval[1];
+
+        List<int[]> list = new ArrayList<>();
+
+        for (int i = 0; i < intervals.length; i++) {
+            int c = intervals[i][0];
+            int d = intervals[i][1];
+
+            if (d < a) {
+                list.add(new int[]{c, d});
+            }
+            else if (c > b) {
+                list.add(new int[]{a, b});
+                a = c;
+                b = d;
+            }
+            else {
+                a = Math.min(a, c);
+                b = Math.max(b, d);
+            }
+        }
+
+        list.add(new int[]{a, b});
+
+        return list.toArray(new int[list.size()][]);
+    }
+}

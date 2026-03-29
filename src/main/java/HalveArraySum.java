80c06a3d4bc5fd245e40999a4abe294c6f0936d8
Rishabh <rishabhdubey1472004@gmail.com>
2026-03-29 11:10:51 +0530

    solved a question

---
+++ src/main/java/HalveArraySum.java
@@ -0,0 +1,28 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class HalveArraySum {
+
+    public int halveArray(int[] nums) {
+        PriorityQueue<Double> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        double sum = 0;
+        for (int i : nums) {
+            sum += i;
+            pq.add((double) i);
+        }
+
+        sum = sum / 2;
+        int ans = 0;
+
+        while (sum > 0) {
+            double temp = pq.poll();
+            temp = temp / 2;
+            pq.add(temp);
+            sum -= temp;
+            ans++;
+        }
+        return ans;
+    }
+
+}

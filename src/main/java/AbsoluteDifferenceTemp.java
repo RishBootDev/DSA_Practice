a929435226be128d1aaec123effb3063f49b5fd2
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-04 12:59:30 +0530

    solved a question

---
+++ src/main/java/AbsoluteDifferenceTemp.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class AbsoluteDifferenceTemp {
+
+
+    public static ArrayList<Long> absDifOne(long n) {
+
+        ArrayList<Long> ans = new ArrayList<>();
+        for (int i = 10; i <= n; i++) {
+            if(check(i)) ans.add((long)i);
+        }
+
+        return ans;
+    }
+
+    public static boolean check(long n) {
+        String str = String.valueOf(n);
+
+        for (int i = 0; i < str.length() - 1; i++) {
+        //    System.out.println(str.charAt(i+1) - str.charAt(i));
+            if(Math.abs(str.charAt(i+1) - str.charAt(i)) == 1) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(absDifOne(12));
+    }
+}
--- src/main/java/GradingStudents.java
+++ src/main/java/GradingStudents.java
@@ -20,8 +20,6 @@ public class GradingStudents {
             }
 
         }
-
         return ans;
-
     }
 }

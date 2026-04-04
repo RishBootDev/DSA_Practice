87abfca584f9a07ac7c98bffd107e242f71660f7
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-05 00:37:00 +0530

    solved a question

--- src/main/java/HIndex.java
+++ src/main/java/HIndex.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class HIndex {
 
+    // This is HI index 1
     public int hIndex(int[] arr) {
 
         List<Integer> list = new ArrayList<>();
@@ -15,4 +16,42 @@ public class HIndex {
 
         return arr.length;
     }
+
+    // This is HI index 2
+    public int hIndex2(int[] citations) {
+        int j = 0;
+
+        for(int i = citations.length -1; i >=0 ;i--) {
+            if(citations[i] < j + 1) return j;
+
+            j++;
+        }
+
+        return citations.length;
+    }
+
+    // this is the binary search version of the hi index
+    public int hIndex3(int[] citations) {
+
+        int left = 0, right = citations.length - 1;
+        int ans = 0;
+        int n = citations.length;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            int papers = n - mid;
+
+            if (citations[mid] >= papers) {
+                ans = papers;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+
 }

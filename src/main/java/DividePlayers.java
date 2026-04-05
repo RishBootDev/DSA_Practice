3e02d4c11a794aa1ff6aa9df7308c46ad0e913c2
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-06 01:25:03 +0530

    solved a question

--- src/main/java/DividePlayers.java
+++ src/main/java/DividePlayers.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class DividePlayers {
 
@@ -23,4 +21,49 @@ public class DividePlayers {
 
         return ans;
     }
+
+
+    // this is the more optimized version of the above code in terms of time but takes extra space of hashmap
+        public long dividePlayers2(int[] skill) {
+            int n = skill.length;
+            int teams = n / 2;
+
+            long totalSum = 0;
+            for (int s : skill) {
+                totalSum += s;
+            }
+
+            if (totalSum % teams != 0) return -1;
+
+            int target = (int)(totalSum / teams);
+            Map<Integer, Integer> freq = new HashMap<>();
+
+            for (int s : skill) {
+                freq.put(s, freq.getOrDefault(s, 0) + 1);
+            }
+
+            long ans = 0;
+
+            for (int s : skill) {
+                if (freq.get(s) == 0) continue;
+
+                int needed = target - s;
+
+                if (!freq.containsKey(needed) || freq.get(needed) == 0) {
+                    return -1;
+                }
+
+                if (s == needed && freq.get(s) < 2) {
+                    return -1;
+                }
+
+                freq.put(s, freq.get(s) - 1);
+                freq.put(needed, freq.get(needed) - 1);
+
+                ans += (long) s * needed;
+            }
+
+            return ans;
+        }
+
 }

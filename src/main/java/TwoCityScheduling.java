69f88c721cd59c40d673190f7aa6634a282914cc
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-08 00:25:18 +0530

    solved a question

---
+++ src/main/java/TwoCityScheduling.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class TwoCityScheduling {
+
+    public int twoCitySchedCost(int[][] costs) {
+
+        Arrays.sort(costs, (a, b) ->
+                (a[1] - a[0]) - (b[1] - b[0]));
+
+        int n = costs.length;
+        int ans = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            ans += costs[i][1];
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            ans += costs[i][0];
+        }
+
+        return ans;
+    }
+}

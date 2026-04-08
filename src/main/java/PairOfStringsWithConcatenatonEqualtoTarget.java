179ea787ccae14544938d311cff63bfe0bfeaf4a
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-08 22:20:23 +0530

    solved a question

---
+++ src/main/java/PairOfStringsWithConcatenatonEqualtoTarget.java
@@ -0,0 +1,18 @@
+public class PairOfStringsWithConcatenatonEqualtoTarget {
+
+
+    // This solution is accepted as per the given constraints but it can be furthur optimized
+    public int numOfPairs(String[] nums, String target) {
+
+        int count = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            for (int j = 0; j < nums.length; j++) {
+
+                if(i == j) continue;
+                if((nums[i] + nums[j]).equals(target)) count++;
+            }
+        }
+
+        return count;
+    }
+}

b09de77b89cc4c49830d36f157985335b793a914
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-10 01:21:14 +0530

    solved a question

---
+++ src/main/java/ValidTriangleNumber.java
@@ -0,0 +1,24 @@
+public class ValidTriangleNumber {
+
+
+    // this is valid bruteforce approach and got TLE after 232 / 241 testcases passed
+    public int triangleNumber(int[] nums) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int a = nums[i];
+                    int b = nums[j];
+                    int c = nums[k];
+                    if(isTriangle(a, b, c)) count++;
+                }
+            }
+        }
+        return count;
+    }
+    public boolean isTriangle(int a, int b, int c) {
+        return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+}

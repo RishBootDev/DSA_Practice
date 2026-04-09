3c611cdc07dcaf8f399fc627e488da1e1a09046b
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-10 00:25:14 +0530

    solved a question

---
+++ src/main/java/RLEIterator.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+// this is a working approach but got memory limit exceeded
+class RLEIterator {
+
+    private List<Integer> fin;
+    private int idx;
+
+    public RLEIterator(int[] encoding) {
+        this.fin = new ArrayList<>();
+        this.idx = 0;
+
+        for (int i = 0; i < encoding.length -1; i+=2) {
+            int val = encoding[i+1];
+            int freq = encoding[i];
+
+            while (freq --> 0) this.fin.add(val);
+        }
+        // System.out.println(fin);
+    }
+
+    public int next(int n) {
+        this.idx += n;
+
+        if (this.idx > fin.size()) return -1;
+
+        return fin.get(this.idx - 1);
+    }
+}
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator obj = new RLEIterator(encoding);
+ * int param_1 = obj.next(n);
+ */

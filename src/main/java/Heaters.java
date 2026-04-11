9b7a5603a866b3ba29e283a23fd648ff98c9db37
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-11 21:46:21 +0530

    solved a question

---
+++ src/main/java/Heaters.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Heaters {
+
+    public int findRadius(int[] houses, int[] heaters) {
+
+
+
+    }
+
+}

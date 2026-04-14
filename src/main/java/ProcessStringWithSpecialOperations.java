695525dd099b62dffba1c443ddd2d05367309a61
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-15 00:25:42 +0530

    solved a question

---
+++ src/main/java/ProcessStringWithSpecialOperations.java
@@ -0,0 +1,18 @@
+public class ProcessStringWithSpecialOperations {
+
+    public String processStr(String s) {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c != '*' && c != '#' && c != '%') {
+                res.append(c);
+            } else if (c == '#') {
+                res.append(res.toString());
+            } else if (c == '%') {
+                res.reverse();
+            } else {
+                if (res.length() > 0) res.deleteCharAt(res.length() - 1);
+            }
+        }
+        return res.toString();
+    }
+}

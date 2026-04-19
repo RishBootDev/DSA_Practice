28091b7163e5240c16140d00ec78ad5e066fa60b
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-19 14:42:24 +0530

    solved a question

---
+++ src/main/java/LinkedListComponents.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListComponents {
+
+    static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> setG = new HashSet<>();
+        for (int i: G) setG.add(i);
+        int res = 0;
+        while (head != null) {
+            if (setG.contains(head.val) && (head.next == null || !setG.contains(head.next.val))) res++;
+            head = head.next;
+        }
+        return res;
+    }
+}

08d9fb6c05ef5bb8cdecc2643560001f9b1c3d9d
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-20 09:17:51 +0530

    solved a question

---
+++ src/main/java/MinimumIncrementToMakeArrayUnique.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class MinimumIncrementToMakeArrayUnique {
+
+    public int minIncrementForUnique(int[] nums) {
+
+        Arrays.sort(nums);
+        int count = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] <= nums[i - 1]) {
+                int temp = nums[i];
+                nums[i] = nums[i-1] + 1;
+                count += (nums[i]- temp);
+            }
+        }
+        return count;
+    }
+}

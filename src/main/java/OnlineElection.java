eb6ca2bbb5653af4f0fe9c638f672d11b1fef5cb
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-20 12:58:00 +0530

    solved a question

---
+++ src/main/java/OnlineElection.java
@@ -0,0 +1,55 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class OnlineElection {
+
+    private int[] persons;
+    private int[] times;
+
+    public OnlineElection(int[] persons, int[] times) {
+        this.persons = persons;
+        this.times = times;
+    }
+
+    public int q(int t) {
+
+        int idx = getIndex(t);
+        if (idx == -1) return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i <= idx; i++) {
+            map.put(persons[i], map.getOrDefault(persons[i], 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        int leader = 0;
+
+        for (int i = 0; i <= idx; i++) {
+            int p = persons[i];
+            int count = map.getOrDefault(p, 0) + 1;
+            map.put(p, count);
+
+            if (count >= max) {
+                max = count;
+                leader = p;
+            }
+        }
+
+        return leader;
+    }
+
+    public int getIndex(int t) {
+        int idx = -1;
+
+        for (int i = 0; i < times.length; i++) {
+            if (times[i] <= t) {
+                idx = i;
+            } else {
+                break;
+            }
+        }
+
+        return idx;
+    }
+}

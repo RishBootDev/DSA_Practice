0f37c6af7fb6920280b3c1e7719557e61a636cd4
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-25 09:52:09 +0530

    solved a question

--- src/main/java/RandomFlipMatrix.java
+++ src/main/java/RandomFlipMatrix.java
@@ -1,5 +1,6 @@
-
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 //  this approach works fine but passes only 17 out of 20 test cases for this  problem
 public class RandomFlipMatrix {
@@ -37,5 +38,33 @@ public class RandomFlipMatrix {
         i = 0;
         j = 0;
         mat = new int[mat.length][mat[0].length];
+    }
+}
+
+// this is the correct solution and passes all the 20/20 test cases for the problem
+class Solution519 {
+
+    Map<Integer, Integer> map;
+    int rows, cols, total;
+    Random rand;
+
+    public Solution519(int n_rows, int n_cols) {
+        map = new HashMap<>();
+        rand = new Random();
+        rows = n_rows;
+        cols = n_cols;
+        total = rows * cols;
+    }
+
+    public int[] flip() {
+        int r = rand.nextInt(total--);
+        int x = map.getOrDefault(r, r);
+        map.put(r, map.getOrDefault(total, total));
+        return new int[]{x / cols, x % cols};
+    }
+
+    public void reset() {
+        map.clear();
+        total = rows * cols;
     }
 }

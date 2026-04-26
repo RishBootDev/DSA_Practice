e98e682ed89a2c5356bef57882319e223b82fdcb
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-26 15:08:10 +0530

    solved a question

---
+++ src/main/java/ConsecutiveDataStream.java
@@ -0,0 +1,20 @@
+public class ConsecutiveDataStream {
+
+    private int t;
+    private int k;
+    private int value;
+
+    public ConsecutiveDataStream(int value, int k) {
+         this.k = k;
+         this.value = value;
+         this.t = 0;
+    }
+
+    public boolean consec(int num) {
+
+       if(this.value == num) this.t++;
+       else return false;
+
+       return this.t >= this.k;
+    }
+}

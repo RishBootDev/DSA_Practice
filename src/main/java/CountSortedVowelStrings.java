4744d6b64ef58b7e28ca0ed43c8b66e99cea4ccc
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-29 14:00:32 +0530

    solved a question

---
+++ src/main/java/CountSortedVowelStrings.java
@@ -0,0 +1,26 @@
+public class CountSortedVowelStrings {
+
+    Integer[][] dp;
+
+    public int countVowelStrings(int n) {
+
+        dp = new Integer[6][n + 1];
+        return helper(n, 1, 0);
+    }
+
+    public int helper(int n, int i, int len) {
+
+        if(len == n) {
+            return 1;
+        }
+        if(dp[i][len] != null) {
+            return dp[i][len];
+        }
+        int count = 0;
+        for(int k = i; k <= 5; k++) {
+            count += helper(n, k, len + 1);
+        }
+
+        return dp[i][len] = count;
+    }
+}

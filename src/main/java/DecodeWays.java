a058d9466e71b39a61b3c768326b7c51b2909b82
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-29 12:56:51 +0530

    solved a question

--- src/main/java/DecodeWays.java
+++ src/main/java/DecodeWays.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 class DecodeWays {
 
-
     // this is a pure recursion and i got time limit exceeded here
+    // After recursion i am going to apply dp here
     public int numDecodings(String s) {
-        return helper(s, 0);
+        int dp[] = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return helper(s, 0, dp);
     }
 
-    public int helper(String s, int i) {
+    public int helper(String s, int i, int dp[]) {
 
         if(i == s.length()) {
             return 1;
@@ -16,13 +20,13 @@ class DecodeWays {
             return 0;
         }
 
-        int count = helper(s, i + 1);
+        int count = helper(s, i + 1, dp);
 
         if(i + 1 < s.length()) {
             int num = (s.charAt(i) - '0') * 10
                     + (s.charAt(i + 1) - '0');
             if(num >= 10 && num <= 26) {
-                count += helper(s, i + 2);
+                count += helper(s, i + 2, dp);
             }
         }
         return count;

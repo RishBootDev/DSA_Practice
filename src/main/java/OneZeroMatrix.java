a925e876ff0348f8e38a9f2f14b6369f8d619aec
Rishabh <rishabhdubey1472004@gmail.com>
2026-05-01 00:58:40 +0530

    solved a question

--- src/main/java/OneZeroMatrix.java
+++ src/main/java/OneZeroMatrix.java
@@ -1,42 +1,50 @@
 public class OneZeroMatrix {
 
     // this is a first intuition of solving it using dynamic programming
-    public int[][] updateMatrix(int[][] mat) {
+    // this solved the problem by complexity of O(m*n) but still not
+    // optimized for the question
+    // Best approach is Multi Source BFS
+        Integer dp [][];
 
-        int[][] ans = new int[mat.length][mat[0].length];
+        public int[][] updateMatrix(int[][] mat) {
 
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
+            int[][] ans = new int[mat.length][mat[0].length];
 
-                boolean[][] vis =
-                        new boolean[mat.length][mat[0].length];
-                int dist = helper(mat, i, j, vis);
-                ans[i][j] = dist;
+            for (int i = 0; i < mat.length; i++) {
+                for (int j = 0; j < mat[0].length; j++) {
+
+                    boolean[][] vis =
+                            new boolean[mat.length][mat[0].length];
+                    dp = new Integer[mat.length][mat[0].length];
+                    int dist = helper(mat, i, j, vis);
+                    ans[i][j] = dist;
+                }
             }
+            return ans;
         }
-        return ans;
-    }
 
-    public int helper(int[][] mat, int i, int j, boolean[][] vis) {
+        public int helper(int[][] mat, int i, int j, boolean[][] vis) {
 
-        if (i < 0 || j < 0 ||
-                i >= mat.length || j >= mat[0].length) {
-            return 10000;
+            if (i < 0 || j < 0 ||
+                    i >= mat.length || j >= mat[0].length) {
+                return 10000;
+            }
+            if (mat[i][j] == 0) {
+                return 0;
+            }
+            if (vis[i][j]) {
+                return 10000;
+            }
+            if(dp[i][j] != null) return dp[i][j];
+            vis[i][j] = true;
+
+            int up = helper(mat, i - 1, j, vis);
+            int left = helper(mat, i, j - 1, vis);
+            int right = helper(mat, i + 1, j, vis);
+            int down = helper(mat, i, j + 1, vis);
+
+            vis[i][j] = false;
+            return dp[i][j] = Math.min(up, Math.min(left, Math.min(right, down))) + 1;
         }
-        if (mat[i][j] == 0) {
-            return 0;
-        }
-        if (vis[i][j]) {
-            return 10000;
-        }
-        vis[i][j] = true;
 
-        int up = helper(mat, i - 1, j, vis);
-        int left = helper(mat, i, j - 1, vis);
-        int right = helper(mat, i + 1, j, vis);
-        int down = helper(mat, i, j + 1, vis);
-
-        vis[i][j] = false;
-        return Math.min(up, Math.min(left, Math.min(right, down))) + 1;
-    }
 }

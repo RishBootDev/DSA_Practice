51c9c85572fce7e86fa544d035b6a366c68fd8fb
Rishabh <rishabhdubey1472004@gmail.com>
2026-05-01 14:48:21 +0530

    solved a question

--- src/main/java/NumberOfDiceRolls.java
+++ src/main/java/NumberOfDiceRolls.java
@@ -1,6 +1,15 @@
 public class NumberOfDiceRolls {
 
+
+    // this dp approach works well and clearly solves 59/62 test cases
+    // now completely solving this question by taking mod
+        private Integer dp[][];
+        private int MOD = 1_000_000_007;
+
         public int numRollsToTarget(int n, int k, int target) {
+
+            dp = new Integer[n + 1][target + 1];
+
             return helper(n, k, target);
         }
 
@@ -12,12 +21,16 @@ public class NumberOfDiceRolls {
             if (n == 0 || target < 0) {
                 return 0;
             }
-            int count = 0;
-
-            for (int j = 1; j <= k; j++) {
-                count += helper(n - 1, k, target - j);
+            if (dp[n][target] != null) {
+                return dp[n][target];
             }
-            return count;
+
+            int count = 0;
+            for (int j = 1; j <= k; j++) {
+                count = (count + helper(n - 1, k, target - j)) % MOD;
+            }
+
+            return dp[n][target] = count;
         }
 
 }

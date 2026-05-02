3a087c9ae00f4c0b09d2e55b8d319f0e7fbe905f
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-05-02 11:37:13 +0530

    Time: 4 ms (59.28%) | Memory: 42.8 MB (24.21%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,7 +1,9 @@
 class Solution {
 
-    public int rotatedDigits(int n) {
+    Integer dp[];
 
+    public int rotatedDigits(int n) {
+        this.dp = new Integer[n+1];
         int count = 0;
         for (int i = 1; i <= n; i++) {
 
@@ -24,15 +26,17 @@ class Solution {
             return 0;
         }
 
+        if(dp[n] != null) return dp[n];
+
         int left = helper(n / 10);
         int right = helper(n % 10);
 
         if (left == 0 || right == 0) {
-            return 0;
+            return dp[n] = 0;
         }
         if (left == 2 || right == 2) {
-            return 2;
+            return dp[n] = 2;
         }
-        return 1;
+        return dp[n] = 1;
     }
 }

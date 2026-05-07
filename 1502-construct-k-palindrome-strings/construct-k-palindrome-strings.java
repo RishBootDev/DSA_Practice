cdaf0cd231a4d672ac220a6b74f52a3eba9b0765
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-05-07 23:26:51 +0530

    Time: 31 ms (11.34%) | Memory: 47.8 MB (11.34%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    
+    public boolean canConstruct(String s, int k) {
+        if(s.length() < k) return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i),
+                    map.getOrDefault(s.charAt(i), 0) +1);
+        }
+
+        int oddCount = 0;
+        for(Integer ent : map.values()) {
+            if(ent % 2 == 1) oddCount ++;
+        }
+
+        if(oddCount > k) return false;
+        return true;
+    }
+}

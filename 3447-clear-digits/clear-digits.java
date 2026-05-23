7e0dd939a812222bb67fe315242e58c770ade6f7
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-05-23 10:01:36 +0530

    Time: 2 ms (76.52%) | Memory: 44.1 MB (24.79%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String clearDigits(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch >= '0' && ch <= '9') {
+                if(!stack.isEmpty()) stack.pop();
+            }else stack.push(ch);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char ch : stack) sb.append(ch);
+
+        return sb.toString();
+    }
+}

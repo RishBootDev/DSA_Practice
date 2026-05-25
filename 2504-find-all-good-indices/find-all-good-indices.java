8f6a9403300095e46b20ed00c2f0a0c3dc4a7fdf
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-05-25 23:28:58 +0530

    Time: 7 ms (42.59%) | Memory: 99.1 MB (78.15%) - LeetSync

---
+++ 2504-find-all-good-indices/find-all-good-indices.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public List<Integer> goodIndices(int[] nums, int k) {
+
+        int n = nums.length;
+
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+
+        pre[0] = 1;
+        for (int i = 1; i < n; i++) {
+
+            if (nums[i] <= nums[i - 1]) {
+                pre[i] = pre[i - 1] + 1;
+            } else {
+                pre[i] = 1;
+            }
+        }
+
+        suf[n - 1] = 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] <= nums[i + 1]) {
+                suf[i] = suf[i + 1] + 1;
+            } else {
+                suf[i] = 1;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = k; i < n - k; i++) {
+
+            if (pre[i - 1] >= k &&
+                suf[i + 1] >= k) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}

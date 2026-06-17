f03eb189b886dfdd6d982e5c86e84b8894d6b573
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-06-17 18:10:45 +0530

    Time: 4 ms (7.53%) | Memory: 43.3 MB (9.59%) - LeetSync

---
+++ 1994-minimum-number-of-swaps-to-make-the-binary-string-alternating/minimum-number-of-swaps-to-make-the-binary-string-alternating.java
@@ -0,0 +1,59 @@
+class Solution {
+   
+   public int minSwaps(String s) {
+
+        int count1 = count(s, '1');
+        int count0 = count(s, '0');
+        if (s.length() % 2 == 0) {
+            if(count1 != count0) return -1;
+            StringBuilder sb1 = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                if(i % 2== 0) {
+                    sb1.append('0');
+                }else sb1.append('1');
+            }
+            StringBuilder sb2 = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                if(i % 2== 1) {
+                    sb2.append('0');
+                }else sb2.append('1');
+            }
+            int temp1 = match(s, sb1.toString());
+            int temp2 = match(s, sb2.toString());
+            return Math.min(temp1, temp2)/2;
+        }else{
+            if(count1 == count0 + 1) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < s.length(); i++) {
+                    if(i % 2==0) sb.append('1');
+                    else sb.append('0');
+                }
+                return match(s, sb.toString())/2;
+            }else if (count0 == count1 + 1) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < s.length(); i++) {
+                    if(i % 2==1) sb.append('1');
+                    else sb.append('0');
+                }
+                return match(s, sb.toString())/2;
+            }else return -1;
+        }
+        
+    }
+    
+    public int match(String s, String sb) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != sb.charAt(i)) count++;
+        }
+        return count;
+    }
+
+    public int count(String s, char ch) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ch) count++;
+        }
+        return count;
+    }
+}

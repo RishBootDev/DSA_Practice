ccb39c8f31c3e5fcbda804a8db8b648c653374a1
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-06-25 12:56:15 +0530

    Time: 1 ms (97.24%) | Memory: 78.8 MB (22.76%) - LeetSync

---
+++ 1346-maximize-the-topmost-element-after-k-moves/maximize-the-topmost-element-after-k-moves.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public int maximumTop(int[] nums, int k) {
+
+        int n = nums.length;
+    
+        if (k == 0) {
+            return nums[0];
+        }
+        if (n == 1) {
+            if (k % 2 == 1) return -1;
+            return nums[0];
+        }
+
+        if (k == 1) {
+            return nums[1];
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < Math.min(n, k - 1); i++) {
+            max = Math.max(max, nums[i]);
+        }
+        if (k < n) {
+            max = Math.max(max, nums[k]);
+        }
+
+        return max;
+    }
+}

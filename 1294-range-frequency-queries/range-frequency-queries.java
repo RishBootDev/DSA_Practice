71d3176386c9d90d1ed14304ef7369bb475d15c1
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-07-04 00:30:02 +0530

    Time: 160 ms (19.52%) | Memory: 177.3 MB (29.88%) - LeetSync

---
+++ 1294-range-frequency-queries/range-frequency-queries.java
@@ -0,0 +1,34 @@
+class RangeFreqQuery {
+
+    Map<Integer, List<Integer>> map;
+
+    public RangeFreqQuery(int[] arr) {
+        map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (!map.containsKey(arr[i]))
+                map.put(arr[i], new ArrayList<>());
+            map.get(arr[i]).add(i);
+        }
+    }
+
+    public int query(int left, int right, int value) {
+
+        if (!map.containsKey(value))
+            return 0;
+        List<Integer> list = map.get(value);
+        int s = Collections.binarySearch(list, left);
+        int e = Collections.binarySearch(list, right);
+
+        if (s < 0)
+            s = (s + 1) * (-1);
+        if (e < 0)
+            e = (e + 2) * (-1);
+        return e - s + 1;
+    }
+}
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery obj = new RangeFreqQuery(arr);
+ * int param_1 = obj.query(left,right,value);
+ */

b13c48475df6ce389df55787a573fc99063efe71
Rishabh <rishabhdubey1472004@gmail.com>
2026-09-14 13:07:29 +0530

    solved questions

--- src/main/java/JumpGame6.java
+++ src/main/java/JumpGame6.java
@@ -1,17 +1,21 @@
 public class JumpGame6 {
 
-    // this is the accepted recursion and passed 5 test cases out of 73
-    // but it game me tle let us memoize it
+    // this is the accepted memoization and passed 58 test cases out of 73
+    // but it gave me tle
+    Integer dp[];
     public int maxResult(int[] nums, int k) {
+        this.dp = new Integer[nums.length];
         return helper(nums,0 ,k) + nums[0];
     }
     public int helper(int nums[], int i, int k) {
         if(i == nums.length - 1) return 0;
 
+        if(dp[i] != null) return dp[i];
+
         int score = Integer.MIN_VALUE;
         for (int j = i + 1; j < i + k; j++) {
            score = Math.max(score, helper(nums, j, k) + nums[j]);
         }
-        return score;
+        return dp[i] = score;
     }
 }

ecd7814ef3653434774afa192360496afabf6541
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-09-22 09:50:42 +0530

    Time: 148 ms (5.30%) | Memory: 60.7 MB (5.30%) - LeetSync

--- 881-loud-and-rich/loud-and-rich.java
+++ 881-loud-and-rich/loud-and-rich.java
@@ -4,42 +4,32 @@ class Solution {
 
         List<List<Integer>> graph = new ArrayList<>();
 
-        for(int i = 0; i < quiet.length; i++) {
-            graph.add(new ArrayList<>());
+        for (int i = 0; i < quiet.length; i++) {
+           graph.add(new ArrayList<>());
         }
-
-        for(int i = 0; i < richer.length; i++) {
+        for (int i = 0; i < richer.length; i++) {
             int rich = richer[i][0];
             int poor = richer[i][1];
-
             graph.get(poor).add(rich);
         }
-
-        int[] res = new int[quiet.length];
-
-        for(int i = 0; i < quiet.length; i++) {
-
-            boolean[] vis = new boolean[quiet.length];
-
-            PriorityQueue<Integer> pq = new PriorityQueue<>(
-                Comparator.comparingInt(p -> quiet[p])
-            );
-
-            dfs(graph, i, vis, pq);
-
-            res[i] = pq.poll();
+        int res [] = new int[quiet.length];
+        System.out.println(graph);
+        for (int i = 0; i < quiet.length; i++) {
+             boolean vis[] = new boolean[quiet.length];
+             PriorityQueue<Integer> pq = new PriorityQueue<>(
+                     Comparator.comparingInt(p -> quiet[p])
+             );
+             dfs(graph, i, vis, pq);
+             res[i] = pq.isEmpty()?0:pq.poll();
         }
-
         return res;
     }
-
-    public void dfs(List<List<Integer>> graph, int start, boolean[] vis,
-                    PriorityQueue<Integer> pq) {
+    public void dfs(List<List<Integer>> graph, int start, boolean vis[], PriorityQueue<Integer> pq) {
 
         vis[start] = true;
         pq.add(start);
 
-        for(int node : graph.get(start)) {
+        for (int node : graph.get(start)) {
             if(!vis[node]) {
                 dfs(graph, node, vis, pq);
             }
